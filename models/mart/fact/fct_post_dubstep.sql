840e5675597a407ccea17daaf7d84e3ad849d968
the.robot.heisenberg@gmail.com <the.robot.heisenberg@gmail.com>
2023-10-29 15:17:46 +0000

    fct table generate

--- models/mart/fact/fct_post_dubstep.sql
+++ models/mart/fact/fct_post_dubstep.sql
@@ -1,6 +1,9 @@
 SELECT 
     -- generate pk for fct
-     --- {{ dbt_utils.generate_surrogate_key(['artist_id', 'album_id', 'track_id']) }} as fct_key,
-    *
-
-FROM {{ref('int_artists')}}
+    {{ dbt_utils.generate_surrogate_key(['artists.artist_id', 'albums.album_id', 'tracks.track_id']) }} as fct_key,
+    {{ dbt_utils.generate_surrogate_key(['albums.album_id']) }} as album_key,
+    {{ dbt_utils.generate_surrogate_key(['artists.artist_id']) }} as artist_key,
+    {{ dbt_utils.generate_surrogate_key(['tracks.track_id']) }} as track_key,
+FROM {{ref('int_tracks')}} tracks
+LEFT JOIN {{ref('int_albums')}} albums ON tracks.album_id = albums.album_id
+LEFT JOIN {{ref('int_artists')}} artists ON albums.artist_id = artists.artist_id
