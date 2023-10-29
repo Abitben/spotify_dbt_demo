1a01241b0d376bc8898b716ff2809aeb229cd610
the.robot.heisenberg@gmail.com <the.robot.heisenberg@gmail.com>
2023-10-29 20:40:34 +0000

    color is done

---
+++ models/sources/final_query_from_source.sql
@@ -0,0 +1,16 @@
+
+
+SELECT
+    artists.name,
+    AVG(tracks_pop.popularity) as avg_tracks_pop
+FROM 
+    {{source('raw_sources','raw_artists')}} artists
+    LEFT JOIN {{source('raw_sources','raw_albums')}} albums ON albums.artist_id = artists.artist_id
+    LEFT JOIN {{source('raw_sources','raw_albums_tracks')}} tracks ON tracks.album_id = albums.album_id 
+    LEFT JOIN {{source('raw_sources','raw_tracks_pop')}} tracks_pop ON tracks_pop.track_id = tracks.track_id
+
+GROUP BY 
+    artists.name
+
+ORDER BY
+    avg_tracks_pop DESC
